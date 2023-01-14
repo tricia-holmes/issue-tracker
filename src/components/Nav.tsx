ad54288ef1b6260d21f452f77fe5be040c0ca870
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-14 17:15:48 -0600

    Add links to Navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,9 +2,17 @@ import logo from '../assets/gradient.png'
 
 export default function Nav() {
   return (
-    <nav className='navbar has-shadow is-warning'>
+    <nav className='navbar has-shadow is-primary'>
       <div className='navbar-brand'>
-        <img src={logo} alt='placeholder-icon' style={{ maxHeight: '70px' }} className='py-2 px-2' />
+        <a className='navbar-item'>
+          <img src={logo} alt='placeholder-icon' style={{ maxHeight: '70px' }} className='py-2 px-2' />
+        </a>
+      </div>
+      <div className='navbar-menu'>
+        <div className='navbar-end'>
+          <a className='navbar-item'>Project</a>
+          <a className='navbar-item'>Dashboard</a>
+        </div>
       </div>
     </nav>
   )
