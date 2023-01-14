f9f8e3b9f6df3eb1e12274f7fb933736f5e92130
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-14 17:14:08 -0600

    Create Swimlane component

---
+++ src/components/Swimlane.tsx
@@ -0,0 +1,33 @@
+export default function Swimlane() {
+  return (
+    <section className='column is-narrow'>
+      <article className='message is-warning'>
+        <div className='message-header'>
+          <p>Backlog</p>
+        </div>
+        <div className='message-body'>
+          <div className='board-item'>
+            <div className='board-item-content'>
+              <span>Example of a ticket</span>
+            </div>
+          </div>
+          <div className='board-item'>
+            <div className='board-item-content'>
+              <span>Example of a ticket</span>
+            </div>
+          </div>
+          <div className='board-item'>
+            <div className='board-item-content'>
+              <span>Example of a ticket</span>
+            </div>
+          </div>
+          <div className='board-item'>
+            <div className='board-item-content'>
+              <span>Example of a ticket</span>
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
+  )
+}
