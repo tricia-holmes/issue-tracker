e09f25e89af9f8749983918d06a6aae9226c5fe2
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-14 17:14:56 -0600

    Add Swimlane to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import Nav from './components/Nav'
+import Swimlane from './components/Swimlane'
 
 function App() {
-  return <Nav />
+  return (
+    <>
+      <Nav />
+      <div className='container'>
+        <Swimlane />
+      </div>
+    </>
+  )
 }
 
 export default App
