d109cd2bab3c955090a310d7a2f263f5858a79a6
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-14 22:39:39 -0600

    Add state to Swimlane component

--- src/components/Swimlane.tsx
+++ src/components/Swimlane.tsx
@@ -1,14 +1,17 @@
+import { useSelector } from 'react-redux'
+import { Store } from '../store/types'
 import { SwimlaneProps } from '../types/types'
 import Ticket from './Ticket'
 
-export default function Swimlane({type, tickets, color} : SwimlaneProps) {
+export default function Swimlane({type, color} : SwimlaneProps) {
+  const tickets = useSelector((state: Store) => state[type])
 
   const swimlaneTickets = tickets.map((ticket, index) => (
     <Ticket key={`ticket ${index}`}id={ticket.id} title={ticket.title} description={ticket.description} status={ticket.status} />
   ))
 
   return (
-    <section className='column is-narrow'>
+    <section className='column'>
       <article className={`message is-${color}`}>
         <div className='message-header'>
           <p>{type}</p>
