a1773c4d20cad5405e3a2607253ee8d58c3683de
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-14 18:03:27 -0600

    Make status for Swimlanes dynamic

--- src/components/Swimlane.tsx
+++ src/components/Swimlane.tsx
@@ -1,29 +1,7 @@
+import { SwimlaneProps } from '../types/types'
 import Ticket from './Ticket'
 
-export default function Swimlane() {
-  const tickets = [
-    {
-      id: 1,
-      title: 'Error message displayed when trying to submit form',
-      description:
-        'Users are reporting an issue when trying to submit a form on the website. Upon clicking the submit button, an error message appears reading "An error has occurred, please try again later." This issue is affecting a number of users and is preventing them from submitting the form successfully.',
-      status: 'Backlog',
-    },
-    {
-      id: 2,
-      title: 'Website not responsive on mobile devices',
-      description:
-        'Users are reporting an issue when trying to submit a form on the website. Upon clicking the submit button, an error message appears reading "An error has occurred, please try again later." This issue is affecting a number of users and is preventing them from submitting the form successfully.',
-      status: 'Backlog',
-    },
-    {
-      id: 3,
-      title: 'Broken links found on the contact page',
-      description:
-        'Users are reporting an issue when trying to submit a form on the website. Upon clicking the submit button, an error message appears reading "An error has occurred, please try again later." This issue is affecting a number of users and is preventing them from submitting the form successfully.',
-      status: 'Backlog',
-    },
-  ]
+export default function Swimlane({type, tickets} : SwimlaneProps) {
 
   const swimlaneTickets = tickets.map((ticket, index) => (
     <Ticket key={`ticket ${index}`}id={ticket.id} title={ticket.title} description={ticket.description} status={ticket.status} />
@@ -33,7 +11,7 @@ export default function Swimlane() {
     <section className='column is-narrow'>
       <article className='message is-warning'>
         <div className='message-header'>
-          <p>Backlog</p>
+          <p>{type}</p>
         </div>
         <div className='message-body'>{swimlaneTickets}</div>
       </article>
