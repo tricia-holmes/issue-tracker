0b67dfa638f36aabe568f3ebd24758293720c004
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-14 22:36:18 -0600

    Add Redux hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import './App.css'
 import Board from './components/Board'
 import Nav from './components/Nav'
+import { addTicket } from './store/actions'
 
 function App() {
+  const dispatch = useDispatch()
+  const makeTicket = (title: string, description: string) => {
+    dispatch(addTicket({ title, description }))
+  }
+
   return (
     <>
       <Nav />
       <Board />
+      <button onClick={() => makeTicket('this is my title', 'this is a desc')}>Click me</button>
     </>
   )
 }
