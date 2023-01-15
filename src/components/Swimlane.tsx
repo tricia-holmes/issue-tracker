adfde6fa1dea45b15d7dec5bc640f642b09e4cf2
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-15 17:34:26 -0600

    Update Swimlane to get tickets from state

--- src/components/Swimlane.tsx
+++ src/components/Swimlane.tsx
@@ -1,10 +1,10 @@
 import { useSelector } from 'react-redux'
-import { Store } from '../store/types'
+import { RootState } from '../store/store'
 import { SwimlaneProps } from '../types/types'
 import Ticket from './Ticket'
 
 export default function Swimlane({type, color} : SwimlaneProps) {
-  const tickets = useSelector((state: Store) => state[type])
+  const tickets = useSelector((state: RootState) => state.tickets[type])
 
   const swimlaneTickets = tickets.map((ticket, index) => (
     <Ticket key={`ticket ${index}`}id={ticket.id} title={ticket.title} description={ticket.description} status={ticket.status} />
