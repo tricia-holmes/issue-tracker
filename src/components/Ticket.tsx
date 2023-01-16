7fa92970a29e2846673b4297e6e8841cb511ede5
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-16 10:03:18 -0600

    Add edit form conditional rendering

--- src/components/Ticket.tsx
+++ src/components/Ticket.tsx
@@ -1,11 +1,13 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDrag } from 'react-dnd'
 import { useDispatch } from 'react-redux'
 import { getTickets, updateTicket } from '../features/tickets/ticketsSlice'
 import { AppDispatch } from '../store/store'
 import { ItemTypes, TicketProps } from '../types/types'
+import EditForm from './EditForm'
 
 export default function Ticket({ id, title, description, status }: TicketProps) {
+  const [isOpen, setIsOpen] = useState(false)
   const dispatch = useDispatch<AppDispatch>()
   const [{ isDragging }, drag] = useDrag(() => ({
     type: ItemTypes.TICKET,
@@ -22,28 +24,31 @@ export default function Ticket({ id, title, description, status }: TicketProps) 
     }),
   }))
 
-  const test = () => {
-    dispatch(updateTicket({ id, title: 'hello', description: 'its crazy, how tired i am', status }))
+  const toggle = () => {
+    setIsOpen(!isOpen)
   }
 
   return (
-    <div
-      ref={drag}
-      style={{
-        opacity: isDragging ? 0.5 : 1,
-        fontSize: 25,
-        fontWeight: 'bold',
-        cursor: 'move',
-      }}
-    >
-      <div className='ticket'>
-        <div className='ticket-content'>
-          <button className='icon position' onClick={() => test()}>
-            <i className='fa-solid fa-pencil' aria-hidden='true'></i>
-          </button>
-          <span>{title}</span>
+    <>
+      {isOpen && <EditForm close={toggle} id={id} title={title} description={description} status={status} />}
+      <div
+        ref={drag}
+        style={{
+          opacity: isDragging ? 0.5 : 1,
+          fontSize: 25,
+          fontWeight: 'bold',
+          cursor: 'move',
+        }}
+      >
+        <div className='ticket'>
+          <div className='ticket-content'>
+            <button className='icon position' onClick={toggle}>
+              <i className='fa-solid fa-pencil' aria-hidden='true'></i>
+            </button>
+            <span>{title}</span>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
