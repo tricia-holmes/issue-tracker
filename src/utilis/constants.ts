cf066b7458c78961fe6652959c2f58cafb2fbbd2
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-16 00:32:52 -0600

    Add update ticket url

--- src/utilis/constants.ts
+++ src/utilis/constants.ts
@@ -2,5 +2,6 @@ const API_URL = import.meta.env.VITE_API_URL
 
 export const API_ROUTES = {
   GET_TICKETS: `${API_URL}/tickets`,
-  ADD_TICKET: `${API_URL}/tickets`
+  ADD_TICKET: `${API_URL}/tickets`,
+  UPDATE_TICKET: `${API_URL}/tickets/`,
 }
