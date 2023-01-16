2db1f26c6686234f3781b7cd085690794e3428ac
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-16 13:12:45 -0600

    Add additional app routes

--- src/utilis/constants.ts
+++ src/utilis/constants.ts
@@ -2,6 +2,14 @@ const API_URL = import.meta.env.VITE_API_URL
 
 export const API_ROUTES = {
   GET_TICKETS: `${API_URL}/tickets`,
+  GET_SINGLE_TICKET: `${API_URL}/tickets`,
   ADD_TICKET: `${API_URL}/tickets`,
   UPDATE_TICKET: `${API_URL}/tickets/`,
+}
+
+export const APP_ROUTES = {
+  PROJECT: '/',
+  DASH: '/dashboard',
+  ADD: '/tickets',
+  UPDATE: '/tickets/:id'
 }
