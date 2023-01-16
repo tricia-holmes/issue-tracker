316b3e02bfdeb289b4111444019b4f9fd77bfbd7
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-15 23:35:51 -0600

    Add ticket button and open modal functionality

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,16 +1,32 @@
+import { useState } from 'react'
+import AddModal from './AddModal'
 import Swimlane from './Swimlane'
 
 export default function Board() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <section className='container cards-container'>
-      <div className='level-item'>
-        <div className='columns is-mobile is-centered is-multiline board-container'>
-          <Swimlane type='backlog' color='waring' />
-          <Swimlane type='inProgress' color='info'/>
-          <Swimlane type='codeReview' color='danger'/>
-          <Swimlane type='done' color='success'/>
-        </div>
+    <>
+      {isOpen && <AddModal close={toggle}/>}
+      <div className='navbar-end'>
+        <button className='button is-info is-light mt-5 mr-4' onClick={toggle}>
+          Add Ticket
+        </button>
       </div>
-    </section>
+      <section className='container cards-container'>
+        <div className='level-item'>
+          <div className='columns is-mobile is-centered is-multiline board-container'>
+            <Swimlane type='backlog' color='waring' />
+            <Swimlane type='inProgress' color='info' />
+            <Swimlane type='codeReview' color='danger' />
+            <Swimlane type='done' color='success' />
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
